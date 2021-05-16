de571716b13b473b47fd9b88d713738b944663a5
sebiiV <rishimagudia@protonmail.com>
2021-05-16 13:11:52 +0100

    Initial commit. Hello world project template Kernel32 finding and GetProcAddress implemented

---
+++ README.md
@@ -0,0 +1,45 @@
+# NASM snippets
+
+Getting more into assembly and finding the documentation and the the capability of MASM pretty limiting. Hence the switch to NASM.
+
+> NASM is Intel syntax.
+> OPERATION dst,src
+
+
+# Project Setup
+## Prequisites 
+    NASM ‘compiler’ from its Home Site.
+    VSNASM an open-source project to integrate it into Visual Studio from GitHub.
+
+## Setting up Visual Studio
+
+Start Visual Studio and:
+
+    open ‘Tools‘->’Options…‘ window from the main menu.
+    go to ‘Projects and Solutions‘ -> ‘VC++ Project Settings‘.
+    set ‘Build Customization Search Path‘ with your NASM installation folder.
+    confirm and exit.
+
+The last step is to enable .asm support into your project.
+
+    open your project.
+    right-click each project using .asm files.
+    select ‘Build Dependencies‘->’Build Customizations‘.
+    check ‘nasm‘ option.
+    confirm.
+
+Then for each .asm file, you have to:
+
+    open the file ‘Properties‘
+    set into ‘General‘->’Item Type‘ to ‘Netwide Assembler‘ (or whatever you set, “NASM – Netwide Assembler” in my case).
+    enable the debug information for the debug profile from the specific NASM options.
+    confirm.
+
+> NOTE: from this last property window, we can also set if to compile the file or not in 32/64 bits target platforms.
+
+You may also need to change the entrypoint:
+	
+	Right click on the project and go to properties
+	Under Linker --> Advanced you can change the Entry Point
+	This is typically main
+
---
+++ primitiveShell/main.asm
@@ -0,0 +1,80 @@
+BITS 64
+SECTION .text
+global main
+
+main:
+	sub RSP,0x28					; 40 bytes of shadow space
+	and RSP,0FFFFFFFFFFFFFFF0h		; align the stack to a multiple of 16.
+	call find_kernel32				; base addr of dll is returned in RBX
+	call find_getProcAddress		; function addr is returned in RDI
+
+; Finding Kernel32.dll
+;--------------------------------------------------------------------
+; Takes no arguments, returns base address in RBX
+;	- Find the TEB. Located at gs:[0x00]
+;	- The TEB at 0x60 has a pointer to the PEB
+;	- Contains Loader at 0x18
+;	- which contains InMemoryOrder list of modules at 0x20
+;	- The third entry in the list will be kernel32
+;	- 0x20 off this will be the base adress
+;--------------------------------------------------------------------
+find_kernel32:
+	xor rcx,rcx						; RCX = 0
+	mov rax, [gs:rcx+0x60]			; RAX = PEB
+	mov rax, [rax+0x18]				; RAX = PEB->ldr
+	mov rsi, [rax+0x020]			; RSI = PEB->ldr.inMemoryOrder
+	; https://www.felixcloutier.com/x86/lods:lodsb:lodsw:lodsd:lodsq
+	lodsq							; RAX = second module (ntdll.dll)
+	xchg rax,rsi					; RAX = RSI, RSI=RAX
+	lodsq							; RAX = third module (kernel32.dll)
+	mov rbx, [rax+0x020]			; RBX = kernel32.dll base address
+
+; Finding GetProcAddress
+;--------------------------------------------------------------------
+; Takes base address in RBX, returns value in RDI
+;	- Go to the PE header from RCX+0x3c
+;	- Export table at offset 0x88
+;	- Names table at offset 0x20
+;	- Get the function name and string check
+;   - Go to the ordinals table at offset 0x24
+;	- Get function number
+;	- Address table at offset 0x1c
+;	- Return the function address
+; we liberally use 32 bit registers  to ensure we only take the offset
+;--------------------------------------------------------------------
+find_getProcAddress:
+	xor r8 ,r8						; r8 = 0
+	mov r8d,[rbx+0x3c]				; r8d = DOS->e_lfanew offset
+	mov rdx,r8						; RDX = DOS->e_lfanew
+	add rdx,rbx						; RDX = PE header
+	mov r8d,[rdx+0x88]				; r8d = export table offset
+	add r8 ,rbx						; r8 = export table
+	xor rsi,rsi						; RSI = 0
+	mov esi, [r8 + 0x20]			; RSI = name table offset
+	add rsi,rbx						; RSI = names table
+	xor rcx,rcx						; RCX = 0. This will be the ordinal counter
+	mov r9, 0x41636f7250746547		; r9 = "GetProcA"
+	; At this point:
+	;	RBX = kernel32 base addr
+	;	RDX = PE header
+	;	RSI = names table
+	;	r8 = export table addr
+	;	r9 = "GetProcA"
+	get_function:
+		inc rcx						; increment the ordinal counter
+		xor rax, rax				; RAX = 0
+		mov eax, [rsi + rcx * 4]	; get name offset
+		add rax, rbx				; get function name
+		cmp QWORD [rax],r9			; Have we found GetProcAddress ?
+		jnz get_function			; we did not find it.
+		xor rsi, rsi				; RSI = 0
+		mov esi, [r8 + 0x24]		; ESI = ordinals offset
+		add rsi, rbx				; RSI = ordinals table
+		mov cx, [rsi + rcx * 2]		; number of function.
+		xor rsi, rsi				; RSI = 0
+		mov esi, [r8 + 0x1c]		; ESI = address table offset
+		add rsi, rbx				; ESI = address table
+		xor rdx, rdx				; RDX=0
+		mov edx, [rsi + rcx * 4]	; EDX = Pointer offset
+		add rdx, rbx				; RDX = GetProcAddress
+		mov rdi, rdx				; Save GetProcAddress in RDI
